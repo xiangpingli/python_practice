b76b683af38cb1cdb5ed8a0db2dd49e77c97971e
李祥平 <lixiangping@hikvision.com.cn>
2016-06-15 06:37:32 -0700

    unicode 中文测试

--- unicode_test.py
+++ unicode_test.py
@@ -13,6 +13,8 @@ def unicode_test():
   str2=raw_input("请输入中文：".decode('utf-8').encode('utf-8'))
   print u"%s" % str2
 
+  str3=raw_input(u"请输入中文：")
+  print u"%s" % str3
 
 if __name__=="__main__":
   unicode_test()
